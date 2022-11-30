703943524e7ef4514b0f14d9f33c7376902a58be
FruitRiin <nassii74@gmail.com>
2022-11-30 19:43:30 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts">
+<script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { PushNotifications } from '@capacitor/push-notifications';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSMtIGHiynM8NjH_gF8Ze3V2Lwc9YIgxs",
+  authDomain: "missriin.firebaseapp.com",
+  projectId: "missriin",
+  storageBucket: "missriin.appspot.com",
+  messagingSenderId: "574411188297",
+  appId: "1:574411188297:web:4af349d8e876a6042c3798"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const addListeners = async () => {
+  await PushNotifications.addListener('registration', token => {
+    console.info('Registration token: ', token.value);
+  });
+
+  await PushNotifications.addListener('registrationError', err => {
+    console.error('Registration error: ', err.error);
+  });
+
+  await PushNotifications.addListener('pushNotificationReceived', notification => {
+    console.log('Push notification received: ', notification);
+  });
+
+  await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+    console.log('Push notification action performed', notification.actionId, notification.inputValue);
+  });
+}
+
+const registerNotifications = async () => {
+  let permStatus = await PushNotifications.checkPermissions();
+
+  if (permStatus.receive === 'prompt') {
+    permStatus = await PushNotifications.requestPermissions();
+  }
+
+  if (permStatus.receive !== 'granted') {
+    throw new Error('User denied permissions!');
+  }
+  await PushNotifications.register();
+}
+addListeners()
+registerNotifications()
+const getDeliveredNotifications = async () => {
+  const notificationList = await PushNotifications.getDeliveredNotifications();
+  console.log('delivered notifications', notificationList);
+}
+getDeliveredNotifications()
 </script>
 
 <template>
@@ -9,8 +68,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    0.0.1
     <a href="https://vuejs.org/" target="_blank">
-      <img src="assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
