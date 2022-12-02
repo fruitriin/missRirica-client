570563d9574d4b9b044a103d4db36beb3d1894b4
FruitRiin <nassii74@gmail.com>
2022-12-03 00:11:30 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 // Import the functions you need from the SDKs you need
 import { PushNotifications } from '@capacitor/push-notifications';
-// import OneSignal from 'onesignal-cordova-plugin'
-//
-//
-// // Call this function when your app starts
-// function OneSignalInit() {
-//   // Uncomment to set OneSignal device logging to VERBOSE
-//   // OneSignal.setLogLevel(6, 0);
-//
-//   // NOTE: Update the setAppId value below with your OneSignal AppId.
-//   OneSignal.setAppId();
-//   OneSignal.setNotificationOpenedHandler(function(jsonData) {
-//     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-//   });
-//
-//   // Prompts the user for notification permissions.
-//   //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
-//   OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
-//     console.log("User accepted notifications: " + accepted);
-//   });
-// }
 
 const addListeners = async () => {
   await PushNotifications.addListener('registration', token => {
