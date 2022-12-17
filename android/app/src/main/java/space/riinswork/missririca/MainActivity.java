cbc3061e9508548e9ae512410ea15f2c9811638c
FruitRiin <nassii74@gmail.com>
2022-12-17 18:35:17 +0900

    諸々の調整 1.0(7)

--- android/app/src/main/java/space/riinswork/missririca/MainActivity.java
+++ android/app/src/main/java/space/riinswork/missririca/MainActivity.java
@@ -1,21 +1,6 @@
 package space.riinswork.missririca;
 
 import com.getcapacitor.BridgeActivity;
-import com.onesignal.OneSignal;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Enable verbose OneSignal logging to debug issues if needed.
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
-
-        // OneSignal Initialization
-        OneSignal.initWithContext(this);
-        OneSignal.setAppId("26c23e85-1fc8-4115-8cf3-f81338427bf3");
-
-        // promptForPushNotifications will show the native Android notification permission prompt.
-        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
-        OneSignal.promptForPushNotifications();
-    }
 }
