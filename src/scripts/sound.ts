c741bfe49418bca27e2470812201082d69b96540
FruitRiin <nassii74@gmail.com>
2023-01-25 11:27:14 +0900

    fix sound

--- src/scripts/sound.ts
+++ src/scripts/sound.ts
@@ -34,33 +34,42 @@ export const soundsTypes = [
 	'noizenecio/kick_gaba7',
 ] as const;
 
-export function getAudio(file: string, useCache = true): HTMLAudioElement {
-	let audio: HTMLAudioElement;
+
+export async function getAudio(file: string, useCache = true) {
 	if (useCache && cache.has(file)) {
-		audio = cache.get(file);
-	} else {
-		audio = new Audio(`/client-assets/sounds/${file}.mp3`);
-		if (useCache) cache.set(file, audio);
+		return cache.get(file);
 	}
-	return audio;
+
+	const response = await fetch(`/client-assets/sounds/${file}.mp3`);
+	const arrayBuffer = await response.arrayBuffer()
+	const audioBuffer = await ctx.decodeAudioData(arrayBuffer)
+	if (useCache) cache.set(file, audioBuffer);
+	return audioBuffer;
 }
 
-export function setVolume(audio: HTMLAudioElement, volume: number): HTMLAudioElement {
-	const masterVolume = ColdDeviceStorage.get('sound_masterVolume');
-	audio.volume = masterVolume - ((1 - volume) * masterVolume);
+export function setVolume(
+	audio: HTMLAudioElement,
+	volume: number
+): HTMLAudioElement {
+	const masterVolume = ColdDeviceStorage.get("sound_masterVolume");
+
+	audio.volume = masterVolume - (1 - volume) * masterVolume;
 	return audio;
 }
 
 export function play(type: string) {
-	const sound = ColdDeviceStorage.get('sound_' + type as any);
+	const sound = ColdDeviceStorage.get(("sound_" + type) as any);
 	if (sound.type == null) return;
 	playFile(sound.type, sound.volume);
 }
 
-export function playFile(file: string, volume: number) {
-	const masterVolume = ColdDeviceStorage.get('sound_masterVolume');
+export async function playFile(file: string, volume: number) {
+	const masterVolume = ColdDeviceStorage.get("sound_masterVolume");
 	if (masterVolume === 0) return;
 
-	const audio = setVolume(getAudio(file), volume);
-	audio.play();
+	const soundSource = ctx.createBufferSource()
+	soundSource.buffer = await getAudio(file)
+	soundSource.connect(ctx.destination)
+	soundSource.start()
+
 }
