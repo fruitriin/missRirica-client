cf5d8038a1da30db12765325c344e4ab0e4f65fd
FruitRiin <nassii74@gmail.com>
2023-02-13 08:59:46 +0900

    init

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import { miLocalStorage } from "./local-storage";
+import { $i } from "@/account";
+import { langNames } from "@/i18n"
+
+const address = $i? new URL($i.instanceUrl) : null;
+const siteName = (document.querySelector('meta[property="og:site_name"]') as HTMLMetaElement)?.content;
+
+export const host = address?.host;
+export const hostname = address?.hostname;
+export const url = $i?.instanceUrl;
+export const apiUrl = url + '/api';
+export const lang = miLocalStorage.getItem('lang');
+export const langs = langNames
+export const version = _VERSION_;
+export const instanceName = siteName === 'Misskey' ? host : siteName;
+export const ui = miLocalStorage.getItem('ui');
+export const debug = miLocalStorage.getItem('debug') === 'true';
---
+++ src/pages/welcome.vue
@@ -0,0 +1,7 @@
+<template>
+<XEntrance/>
+</template>
+
+<script lang="ts" setup>
+import XEntrance from './welcome.entrance.a.vue';
+</script>
