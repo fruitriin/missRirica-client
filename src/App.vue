0fb0868b604cc8ae8b574c561d8cc003cff0ce3f
FruitRiin <nassii74@gmail.com>
2023-05-04 16:17:54 +0900

    とりあえずログインできるようになった

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,84 @@
 <template>
-  <div>
-    <form class="eppvobhk" :class="{ signing, totpLogin }" @submit.prevent="onSubmit">
-      <div class="auth _gaps_m">
-        <div v-show="withAvatar" class="avatar" :style="{ backgroundImage: user ? `url('${ user.avatarUrl }')` : null, marginBottom: message ? '1.5em' : null }"></div>
-        <MkInfo v-if="message">
-          {{ message }}
-        </MkInfo>
-        <div v-if="!totpLogin" class="normal-signin _gaps_m">
-          <MkInput v-model="username" :placeholder="i18n.ts.username" type="text" pattern="^[a-zA-Z0-9_]+$" :spellcheck="false" autocomplete="username" autofocus required data-cy-signin-username @update:model-value="onUsernameChange">
-            <template #prefix>@</template>
-            <template #suffix>@{{ host }}</template>
-          </MkInput>
-          <MkInput v-if="!user || user && !user.usePasswordLessLogin" v-model="password" :placeholder="i18n.ts.password" type="password" autocomplete="current-password" :with-password-toggle="true" required data-cy-signin-password>
-            <template #prefix><i class="ti ti-lock"></i></template>
-            <template #caption><button class="_textButton" type="button" @click="resetPassword">{{ i18n.ts.forgotPassword }}</button></template>
-          </MkInput>
-          <MkButton type="submit" large primary rounded :disabled="signing" style="margin: 0 auto;">{{ signing ? i18n.ts.loggingIn : i18n.ts.login }}</MkButton>
-        </div>
-        <div v-if="totpLogin" class="2fa-signin" :class="{ securityKeys: user && user.securityKeys }">
-          <div v-if="user && user.securityKeys" class="twofa-group tap-group">
-            <p>{{ i18n.ts.tapSecurityKey }}</p>
-            <MkButton v-if="!queryingKey" @click="queryKey">
-              {{ i18n.ts.retry }}
-            </MkButton>
-          </div>
-          <div v-if="user && user.securityKeys" class="or-hr">
-            <p class="or-msg">{{ i18n.ts.or }}</p>
-          </div>
-          <div class="twofa-group totp-group">
-            <p style="margin-bottom:0;">{{ i18n.ts.twoStepAuthentication }}</p>
-            <MkInput v-if="user && user.usePasswordLessLogin" v-model="password" type="password" autocomplete="current-password" :with-password-toggle="true" required>
-              <template #label>{{ i18n.ts.password }}</template>
-              <template #prefix><i class="ti ti-lock"></i></template>
-            </MkInput>
-            <MkInput v-model="token" type="text" pattern="^[0-9]{6}$" autocomplete="one-time-code" :spellcheck="false" required>
-              <template #label>{{ i18n.ts.token }}</template>
-              <template #prefix><i class="ti ti-123"></i></template>
-            </MkInput>
-            <MkButton type="submit" :disabled="signing" large primary rounded style="margin: 0 auto;">{{ signing ? i18n.ts.loggingIn : i18n.ts.login }}</MkButton>
-          </div>
-        </div>
-      </div>
-    </form>
+  <div v-if="!loggedIn">
+    <div>
+    <label>
+      サーバー名
+      <input v-model="serverHost" placeholder="https://misskey.example.com/" />
+
+    </label>
+    </div>
+
+    <div>
+    <label>
+      アカウント名
+      <input v-model="userName" />
+
+    </label>
+    </div>
+    <div>
+    <label for="">アクセストークン
+    <input v-model="accessToken"  type="password" />
+    </label>
+    </div>
+    <button @click="signIn">ろぐいん！</button>
+    <pre>{{ account }}</pre>
   </div>
 </template>
 
 <script>
+import { api as misskeyApi } from 'misskey-js';
 
+import MkButton from "@/Components/MkButton.vue";
+import MkInput from "@/Components/MkInput.vue";
 
 export default {
+  components: {
+    MkInput,
+    MkButton
+  },
+  mounted(){
+    if(localStorage.getItem("account")) this.activateMisskeyV13()
+  },
 
   data(){
     return {
       i18n: {
         ts: {}
-      }
+      },
+      serverHost: "",
+      userName: "",
+      accessToken: "",
+      account: {},
+      loggedIn: false,
     }
   },
-  mounted() {
+  methods:{
+    signIn(){
+      const cli = new misskeyApi.APIClient({
+        origin: this.serverHost,
+        credential: this.accessToken,
+      });
 
-    const misskeyV13 = document.createElement("script")
-    misskeyV13.setAttribute("src", "misskey-v13/app.js")
-    misskeyV13.setAttribute("type", "module")
-    const misskeyV13Style = document.createElement("link")
-    misskeyV13Style.setAttribute("rel", "stylesheet" )
-    misskeyV13Style.setAttribute("href", "misskey-v13/init.css")
-    document.head.appendChild(misskeyV13)
-    document.head.appendChild(misskeyV13Style)
-  }
+      cli.request("i").then((res) => {
+        this.account = res
+
+        localStorage.setItem("account", JSON.stringify({...res, token: this.accessToken, instanceUrl: this.serverHost}))
+        this.activateMisskeyV13()
+      })
+
+    },
+    activateMisskeyV13(){
+      const misskeyV13 = document.createElement("script")
+      misskeyV13.setAttribute("src", "misskey-v13/app.js")
+      misskeyV13.setAttribute("type", "module")
+      const misskeyV13Style = document.createElement("link")
+      misskeyV13Style.setAttribute("rel", "stylesheet" )
+      misskeyV13Style.setAttribute("href", "misskey-v13/init.css")
+      document.head.appendChild(misskeyV13)
+      document.head.appendChild(misskeyV13Style)
+      this.loggedIn = true
+    }
+  },
 }
 </script>
 
