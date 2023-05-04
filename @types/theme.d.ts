aa2d7c229c81eaa467b0fb5587dcc66a49729fa6
FruitRiin <nassii74@gmail.com>
2023-05-04 09:56:52 +0900

    13.11.3 ririca 0.1

--- @types/theme.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@/themes/*.json5' {
-	import { Theme } from "@/scripts/theme";
-
-	const theme: Theme;
-
-	export default theme;
-}
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    hi
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+
+    const misskeyV13 = document.createElement("script")
+    misskeyV13.setAttribute("src", "dist/misskey-v13/app.js")
+    misskeyV13.setAttribute("type", "module")
+    const misskeyV13Style = document.createElement("link")
+    misskeyV13Style.setAttribute("rel", "stylesheet" )
+    misskeyV13Style.setAttribute("href", "dist/misskey-v13/init.css")
+    document.head.appendChild(misskeyV13)
+    document.head.appendChild(misskeyV13Style)
+  }
+}
+</script>
