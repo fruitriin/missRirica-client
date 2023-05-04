dc5dbaf9c0902de0ce4d3a939310c2b6ede87c07
FruitRiin <nassii74@gmail.com>
2023-05-04 14:41:32 +0900

    fix workflow

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@ export default {
   mounted() {
 
     const misskeyV13 = document.createElement("script")
-    misskeyV13.setAttribute("src", "dist/misskey-v13/app.js")
+    misskeyV13.setAttribute("src", "misskey-v13/app.js")
     misskeyV13.setAttribute("type", "module")
     const misskeyV13Style = document.createElement("link")
     misskeyV13Style.setAttribute("rel", "stylesheet" )
-    misskeyV13Style.setAttribute("href", "dist/misskey-v13/init.css")
+    misskeyV13Style.setAttribute("href", "misskey-v13/init.css")
     document.head.appendChild(misskeyV13)
     document.head.appendChild(misskeyV13Style)
   }
